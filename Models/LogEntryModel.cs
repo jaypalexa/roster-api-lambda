c42496acc1a59e3aaaafd8dc1e46753b621cbffa
JP Alexander <jpalexa@gmail.com>
2020-06-16 18:05:23 +0100

    Added userName to LogEntryModel

--- Models/LogEntryModel.cs
+++ Models/LogEntryModel.cs
@@ -4,6 +4,7 @@
     {
         public string logEntryId { get; set; }      // FROM SERVER:  NOW AS UTC = yyyy-MM-dd-HH-mm-ss-fff
         public string organizationId { get; set; }  // FROM CLIENT
+        public string userName { get; set; }        // FROM CLIENT
         public string entryDateTime { get; set; }   // FROM SERVER:  NOW AS UTC == yyyy-MM-dd HH:mm:ssZ
         public string message { get; set; }         // FROM CLIENT
         public int timestamp { get; set; }          // FROM SERVER:  NOWUNIX TIMESTAMP IN SECONDS

383af2d491115ee6bcbcc49e640d6fd4347d6278
JP Alexander <jpalexa@gmail.com>
2020-07-20 19:53:20 +0100

    Updated NuGet packages

--- Function.cs
+++ Function.cs
@@ -57,22 +57,23 @@ namespace RosterApiLambda
         {
             request.pathParameters.TryGetValue("reportId", out string reportId);
 
-            var body = new RosterResponseBody();
-
-            body.data = reportId switch
+            var body = new RosterResponseBody
             {
-                /* FWC REPORTS (PDF) */
-                "MarineTurtleCaptiveFacilityQuarterlyReportForHatchlings" => await MarineTurtleCaptiveFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
-                "MarineTurtleCaptiveFacilityQuarterlyReportForWashbacks" => await MarineTurtleCaptiveFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
-                "MarineTurtleHoldingFacilityQuarterlyReport" => await MarineTurtleHoldingFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
-                "TaggingDataForm" => await TaggingDataFormReportRequestHandler.Handle(organizationId, request),
+                data = reportId switch
+                {
+                    /* FWC REPORTS (PDF) */
+                    "MarineTurtleCaptiveFacilityQuarterlyReportForHatchlings" => await MarineTurtleCaptiveFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
+                    "MarineTurtleCaptiveFacilityQuarterlyReportForWashbacks" => await MarineTurtleCaptiveFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
+                    "MarineTurtleHoldingFacilityQuarterlyReport" => await MarineTurtleHoldingFacilityQuarterlyReportRequestHandler.Handle(organizationId, request),
+                    "TaggingDataForm" => await TaggingDataFormReportRequestHandler.Handle(organizationId, request),
 
-                /* OTHER REPORTS (HTML) */
-                "HatchlingsAndWashbacksByCountyReport" => await HatchlingsAndWashbacksByCountyReportRequestHandler.Handle(organizationId, request),
-                "TurtleInjuryReport" => await TurtleInjuryReportRequestHandler.Handle(organizationId, request),
-                "TurtleTagReport" => await TurtleTagReportRequestHandler.Handle(organizationId, request),
+                    /* OTHER REPORTS (HTML) */
+                    "HatchlingsAndWashbacksByCountyReport" => await HatchlingsAndWashbacksByCountyReportRequestHandler.Handle(organizationId, request),
+                    "TurtleInjuryReport" => await TurtleInjuryReportRequestHandler.Handle(organizationId, request),
+                    "TurtleTagReport" => await TurtleTagReportRequestHandler.Handle(organizationId, request),
 
-                _ => throw new ArgumentOutOfRangeException(ErrorHelper.InvalidReportId(reportId)),
+                    _ => throw new ArgumentOutOfRangeException(ErrorHelper.InvalidReportId(reportId)),
+                }
             };
 
             return body;
@@ -80,29 +81,30 @@ namespace RosterApiLambda
 
         public async Task<RosterResponseBody> HandleDataRequest(string organizationId, RosterRequest request)
         {
-            var body = new RosterResponseBody();
-
-            body.data = request.resource switch
+            var body = new RosterResponseBody
             {
-                "/last-update" => await LastUpdateDataRequestHandler.Handle(organizationId, request),
-                "/organizations/{organizationId}" => await OrganizationDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtle-list-items" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles/{seaTurtleId}" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles/{seaTurtleId}/sea-turtle-tags" => await SeaTurtleTagDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles/{seaTurtleId}/sea-turtle-tags/{seaTurtleTagId}" => await SeaTurtleTagDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles/{seaTurtleId}/sea-turtle-morphometrics" => await SeaTurtleMorphometricDataRequestHandler.Handle(organizationId, request),
-                "/sea-turtles/{seaTurtleId}/sea-turtle-morphometrics/{seaTurtleMorphometricId}" => await SeaTurtleMorphometricDataRequestHandler.Handle(organizationId, request),
-                "/holding-tanks" => await HoldingTankDataRequestHandler.Handle(organizationId, request),
-                "/holding-tanks/{holdingTankId}" => await HoldingTankDataRequestHandler.Handle(organizationId, request),
-                "/holding-tanks/{holdingTankId}/holding-tank-measurements" => await HoldingTankMeasurementDataRequestHandler.Handle(organizationId, request),
-                "/holding-tanks/{holdingTankId}/holding-tank-measurements/{holdingTankMeasurementId}" => await HoldingTankMeasurementDataRequestHandler.Handle(organizationId, request),
-                "/hatchlings-events" => await HatchlingsEventDataRequestHandler.Handle(organizationId, request),
-                "/hatchlings-events/{hatchlingsEventId}" => await HatchlingsEventDataRequestHandler.Handle(organizationId, request),
-                "/washbacks-events" => await WashbacksEventDataRequestHandler.Handle(organizationId, request),
-                "/washbacks-events/{washbacksEventId}" => await WashbacksEventDataRequestHandler.Handle(organizationId, request),
-                "/log-entries" => await LogEntryDataRequestHandler.Handle(organizationId, request),
-                _ => throw new ArgumentOutOfRangeException(ErrorHelper.InvalidResource(request.resource)),
+                data = request.resource switch
+                {
+                    "/last-update" => await LastUpdateDataRequestHandler.Handle(organizationId, request),
+                    "/organizations/{organizationId}" => await OrganizationDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtle-list-items" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles/{seaTurtleId}" => await SeaTurtleDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles/{seaTurtleId}/sea-turtle-tags" => await SeaTurtleTagDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles/{seaTurtleId}/sea-turtle-tags/{seaTurtleTagId}" => await SeaTurtleTagDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles/{seaTurtleId}/sea-turtle-morphometrics" => await SeaTurtleMorphometricDataRequestHandler.Handle(organizationId, request),
+                    "/sea-turtles/{seaTurtleId}/sea-turtle-morphometrics/{seaTurtleMorphometricId}" => await SeaTurtleMorphometricDataRequestHandler.Handle(organizationId, request),
+                    "/holding-tanks" => await HoldingTankDataRequestHandler.Handle(organizationId, request),
+                    "/holding-tanks/{holdingTankId}" => await HoldingTankDataRequestHandler.Handle(organizationId, request),
+                    "/holding-tanks/{holdingTankId}/holding-tank-measurements" => await HoldingTankMeasurementDataRequestHandler.Handle(organizationId, request),
+                    "/holding-tanks/{holdingTankId}/holding-tank-measurements/{holdingTankMeasurementId}" => await HoldingTankMeasurementDataRequestHandler.Handle(organizationId, request),
+                    "/hatchlings-events" => await HatchlingsEventDataRequestHandler.Handle(organizationId, request),
+                    "/hatchlings-events/{hatchlingsEventId}" => await HatchlingsEventDataRequestHandler.Handle(organizationId, request),
+                    "/washbacks-events" => await WashbacksEventDataRequestHandler.Handle(organizationId, request),
+                    "/washbacks-events/{washbacksEventId}" => await WashbacksEventDataRequestHandler.Handle(organizationId, request),
+                    "/log-entries" => await LogEntryDataRequestHandler.Handle(organizationId, request),
+                    _ => throw new ArgumentOutOfRangeException(ErrorHelper.InvalidResource(request.resource)),
+                }
             };
 
             return body;

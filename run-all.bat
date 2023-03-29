470656614d7d4e9386b8a445ff10fa7027f860ec
JP Alexander <jpalexa@gmail.com>
2023-03-29 17:03:59 +0100

    Upgraded from .NET Core 3.1 to .NET 6.0

--- run-all.bat
+++ run-all.bat
@@ -1,3 +1,3 @@
 dotnet build
 dotnet lambda package
-dotnet lambda deploy-function --profile roster-admin-user --function-name roster-api-lambda --package C:\JPFiles\src\roster-api-lambda\bin\Debug\netcoreapp3.1\roster-api-lambda.zip
+dotnet lambda deploy-function --profile roster-admin-user --function-name roster-api-lambda --package C:\JPFiles\src\roster-api-lambda\bin\Debug\net6.0\roster-api-lambda.zip
